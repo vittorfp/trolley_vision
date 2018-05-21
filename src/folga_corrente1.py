7c7ecd4fab44ef1ffbfa4c3e485c5c9e38b9ff3c
vittorfp <vittorfpereira@gmail.com>
2018-05-21 12:55:02 -0300

    Selecionando a variação do tamanho da folga

--- src/folga_corrente1.py
+++ src/folga_corrente1.py
@@ -19,6 +19,7 @@ cap = cv2.VideoCapture('../img/video2.mp4')
 i = 0
 y = []
 x = []
+tam = []
 while(cap.isOpened()):
 	i += 1
 	ret, frame = cap.read()
@@ -30,6 +31,7 @@ while(cap.isOpened()):
 	frame = cv2.equalizeHist(frame)
 	frame = adjust_gamma(frame, gamma=0.8)
 	frame = cv2.equalizeHist(frame)
+	out   = frame.copy()
 	frame = cv2.adaptiveThreshold(frame,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,15,4)
 	frame = cv2.Canny(frame, 900, 1000)
 	frame = frame[80:120,50:300]
@@ -43,17 +45,31 @@ while(cap.isOpened()):
 	#plt.show()
 	print(frame.shape)
 	a = range(250)
+
 	if y[i-1] < 250000:
 		print('Buraco', i)
 		linha = frame[20,:]
-		dists = np.squeeze(np.argwhere(linha == 255))
-		dists = np.diff(dists)
-		print(dists)
-		plt.plot(linha)
-		plt.show()
 
+		dists = np.squeeze(np.argwhere(linha == 255))
+		dif = np.diff(dists)
+		mx = np.max(dif)
+
+		idx_dif = np.argwhere(dif == mx)
+		init = np.squeeze(dists[idx_dif])
+		end = np.squeeze(dists[idx_dif+1])
+		print(mx)
+		if mx > 100:
+			tam.append(mx)
+			cv2.line(out,(init+50, 20+80),(end+50,20+80),(100,100,100), 3)
+			
+	else:
+		if len(tam) != 0:
+			plt.plot(tam)	
+			plt.show()
+		tam = []
 
 	cv2.imshow("Image", frame)
+	cv2.imshow("Image1", out)
 	if cv2.waitKey(1) & 0xFF == ord('q'):
 		break
 	time.sleep(0.03)
