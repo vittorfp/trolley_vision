ad45f90d09e9c7be441cc0e66db0e06abda3b945
vittorfp <vittorfpereira@gmail.com>
2018-05-21 18:44:23 -0300

    Colocação de cores e análise de tres alturas da corrente

--- src/folga_corrente1.py
+++ src/folga_corrente1.py
@@ -19,19 +19,21 @@ cap = cv2.VideoCapture('../img/video2.mp4')
 i = 0
 y = []
 x = []
-tam = []
+tam1 = []
+tam2 = []
+tam3 = []
 while(cap.isOpened()):
 	i += 1
 	ret, frame = cap.read()
 	if frame is None:
 		break
+	out   = frame.copy()[40:250,70:450]
 	frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)[40:250,70:450]
 	frame = cv2.equalizeHist(frame)
 	frame = adjust_gamma(frame, gamma=0.6)
 	frame = cv2.equalizeHist(frame)
 	frame = adjust_gamma(frame, gamma=0.8)
 	frame = cv2.equalizeHist(frame)
-	out   = frame.copy()
 	frame = cv2.adaptiveThreshold(frame,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,15,4)
 	frame = cv2.Canny(frame, 900, 1000)
 	frame = frame[80:120,50:300]
@@ -43,30 +45,60 @@ while(cap.isOpened()):
 	x.append( i )
 	#plt.plot(x,y)
 	#plt.show()
-	print(frame.shape)
+	#print(frame.shape)
 	a = range(250)
 
 	if y[i-1] < 250000:
 		print('Buraco', i)
-		linha = frame[20,:]
-
-		dists = np.squeeze(np.argwhere(linha == 255))
+		linha1 = frame[15,:]
+		linha2 = frame[20,:]
+		linha3 = frame[25,:]
+		
+		dists = np.squeeze(np.argwhere(linha1 == 255))
 		dif = np.diff(dists)
 		mx = np.max(dif)
-
 		idx_dif = np.argwhere(dif == mx)
 		init = np.squeeze(dists[idx_dif])
 		end = np.squeeze(dists[idx_dif+1])
 		print(mx)
+		
 		if mx > 100:
-			tam.append(mx)
-			cv2.line(out,(init+50, 20+80),(end+50,20+80),(100,100,100), 3)
+			tam1.append(mx)
+			cv2.line(out,(init+50, 15+80),(end+50,15+80),(255,0,0), 2)
+
+		dists = np.squeeze(np.argwhere(linha2 == 255))
+		dif = np.diff(dists)
+		mx = np.max(dif)
+		idx_dif = np.argwhere(dif == mx)
+		init = np.squeeze(dists[idx_dif])
+		end = np.squeeze(dists[idx_dif+1])
+		print(mx)
+
+		if mx > 100:
+			tam2.append(mx)
+			cv2.line(out,(init+50, 20+80),(end+50,20+80),(0,0,255), 2)
+
+		dists = np.squeeze(np.argwhere(linha3 == 255))
+		dif = np.diff(dists)
+		mx = np.max(dif)
+		idx_dif = np.argwhere(dif == mx)
+		init = np.squeeze(dists[idx_dif])
+		end = np.squeeze(dists[idx_dif+1])
+		print(mx)
+
+		if mx > 100:
+			tam3.append(mx)
+			cv2.line(out,(init+50, 25+80),(end+50,25+80),(0,255,0), 2)
 			
 	else:
-		if len(tam) != 0:
-			plt.plot(tam)	
+		if len(tam1) != 0:
+			plt.plot(tam1)	
+			plt.plot(tam2)	
+			plt.plot(tam3)	
 			plt.show()
-		tam = []
+		tam1 = []
+		tam2 = []
+		tam3 = []
 
 	cv2.imshow("Image", frame)
 	cv2.imshow("Image1", out)
